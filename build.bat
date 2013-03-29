393f0ccdb799bdd168ea51f8d363c67fb3825d8e
David Powell <djpowell@djpowell.net>
2013-03-29 11:46:09 +0000

    update build script

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 del /q Output\*
-call "%ProgramFiles(x86)%\Inno Setup 5\iscc.exe" /dconfigure leiningen-installer.iss
+call "%ProgramFiles(x86)%\Inno Setup 5\iscc.exe" /dconfigure leiningen-installer.iss && ^
 call "%ProgramFiles(x86)%\Inno Setup 5\iscc.exe" leiningen-installer.iss
